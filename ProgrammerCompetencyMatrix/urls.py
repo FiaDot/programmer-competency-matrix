a584ee2fdea97954fe87a35179078e4491326a89
fiadot <fiadot@gmail.com>
2014-01-17 21:05:31 +0900

    issue #2, #3

--- ProgrammerCompetencyMatrix/urls.py
+++ ProgrammerCompetencyMatrix/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^survey/$', 'survey.views.main'),
+    url(r'^survey/submit$', 'survey.views.submit'),
 )
--- survey/views.py
+++ survey/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-
+from django.shortcuts import render
 from survey.models import Question 
 
 def main(request):    
-    question = Question.objects.all()
-    return render_to_response("survey.html", dict(question=question, user=request.user))        
+    #question = Question.objects.all()
+    #return render_to_response("survey.html", dict(question=question))
 
-def post(request, pk):
-    return 1
-    
+    question = Question.objects.all()
+    context = {'question_list': question}
+    return render(request, 'survey.html', context)
+
+        
+
+def submit(request):
+    question = Question.objects.all()
+    context = {'question_list': question}
+    return render(request, 'survey.html', context)
 
 
 def statistics(request):
